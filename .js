2cb68925476a64a4269e035d00b55a6b608da493
PatrycjaFilipiuk <patrycjafilipiuk94@gmail.com>
2025-05-14 09:53:25 +0100

    Patrycja's code

---
+++ .js
@@ -0,0 +1,27 @@
+const hamburger = document.getElementById('hamburger');
+const sideMenu = document.getElementById('sideMenu');
+const closeMenu = document.getElementById('closeMenu');
+ 
+hamburger.addEventListener('click', () => {
+  sideMenu.classList.toggle('open');
+});
+ 
+closeMenu.addEventListener('click', () => {
+  sideMenu.classList.remove('open');
+});
+ 
+const fadeEls = document.querySelectorAll('.fade-in');
+ 
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      // Stop view
+      observer.unobserve(entry.target);
+    }
+  });
+}, {
+  threshold: 0.1 // 10% view
+});
+ 
+fadeEls.forEach(el => observer.observe(el));
